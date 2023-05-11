dc3080018af82af4d2a24aa76d9f9094f9bae8b6
Radoslaw Tyszkiewicz <ebisu.usa@gmail.com>
2023-05-11 21:42:00 +0900

    CRUD lecture completed

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,9 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
   def index
     @restaurants = Restaurant.all
   end
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
   def new
     @restaurant = Restaurant.new
@@ -14,15 +14,12 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(@restaurant)
   end
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     redirect_to restaurant_path(@restaurant)
   end
   def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     redirect_to restaurants_path, status: :see_other
   end
@@ -30,5 +27,8 @@ class RestaurantsController < ApplicationController
   private
   def restaurant_params
     params.require(:restaurant).permit(:name, :rating, :address)
+  end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
